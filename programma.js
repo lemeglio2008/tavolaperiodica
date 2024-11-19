893d41247c35562aa550a89dc5882032416a51e2
Irene Parente <irene.parente>
2024-11-19 12:39:54 +0100

    super tavola

--- programma.js
+++ programma.js
@@ -1,16 +1,3 @@
-let idrogeno = {};
-
-idrogeno.simbolo = 'h';
-idrogeno.numero = 1;
-idrogeno.nome = 'idrogeno';
-
-let elio = {};
-
-elio.simbolo = 'He';
-elio.numero = 2;
-elio.nome = 'elio';
-
-let elementi = [idrogeno, elio];
 
 
 for (const element of elementi) {
@@ -19,24 +6,32 @@ for (const element of elementi) {
 
 
 let cella = document.createElement('div');
-cella.style.borderStyle = 'solid'
-cella.style.width = '70px'
-
+cella.style.borderStyle = 'solid';
+cella.style.borderWidth = '0.5px';
+cella.style.width = '90px';
+cella.style.height = '120px'; 
+cella.style.position = 'absolute';
+cella.style.left = element.xpos * 90 + 'px';
+cella.style.top = element.ypos * 120 + 'px';
+cella.style.backgroundColor = '#' + element['cpk-hex']
+cella.style.textAlign = 'center'
+cella.style.boxSizing = 'border-box'
 let numeroAtomico = document.createElement('p');
-numeroAtomico.innerText = element.numero;
+numeroAtomico.innerText = element.number;
 cella.appendChild(numeroAtomico);
 
+
 let simbolo = document.createElement('a');
-simbolo.innerText = element.simbolo;
+simbolo.innerText = element.symbol;
+simbolo.href=element.source;
 cella.appendChild(simbolo);
+simbolo.style.fontSize = '24px'
+simbolo.style.fontWeight = 'bold'
+simbolo.style.color = 'black'
 
 let nome = document.createElement('p');
-nome.innerText = element.nome;
+nome.innerText = element.name;
 cella.appendChild(nome);
 
 contenitore.appendChild(cella);
 }
-
-
-
-
